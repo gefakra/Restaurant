e38754a88844302fb78a00a301a79e9c5c90f2e7
a.soluyanov <a.soluyanov@innopolis.ru>
2023-10-27 21:24:24 +0300

    Добавьте файлы проекта.

---
+++ Menu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public abstract class Item
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Item(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public abstract void Display();
+    }
+
+    public class Drink : Item
+    {
+        public Drink(string name, double price) : base(name, price) { }
+
+        public override void Display()
+        {
+            Console.WriteLine($"Напиток: {Name}, Цена: {Price:C}");
+        }
+    }
+
+    public class Snack : Item
+    {
+        public Snack(string name, double price) : base(name, price) { }
+
+        public override void Display()
+        {
+            Console.WriteLine($"Закуска: {Name}, Цена: {Price:C}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Restaurant;
+
+
+List<Item> menuItems = new List<Item>
+            {
+                new Drink("Чай", 1.5),
+                new Drink("Кофе", 2.0),
+                new Snack("Суп", 3.0),
+                new Snack("Второе", 4.0)
+            };
+
+Console.WriteLine("Меню ресторана:");
+foreach (var item in menuItems)
+{
+    item.Display();
+}
